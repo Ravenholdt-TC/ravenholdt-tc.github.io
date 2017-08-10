fc0b612153c92fdab77799b8b9ebdc449913e262
Florian Meißner <developer@mystler.eu>
2017-08-10 15:04:01 +0200

    Add date updating script

---
+++ UpdateDates.rb
@@ -0,0 +1,54 @@
+puts 'Enter WoW build number:'
+wowbuild = gets.to_i
+
+todayStr = Time.now.strftime("%Y-%m-%d")
+
+updatedFiles = []
+IO.popen('git diff --name-only HEAD').each do |line|
+  updatedFiles << line.chomp if line.chomp.end_with?('.csv')
+end
+puts "#{updatedFiles.length} updated CSV files..."
+puts updatedFiles
+puts
+
+puts 'Updating corresponding HTML files...'
+Dir.glob("combinations/*.html").each do |file|
+  puts "Checking #{file}"
+  text = File.read(file)
+  updatedFiles.each do |csv|
+    if text.include?(csv)
+      new_contents = text.gsub(/\blastupdate: \d{4}-\d{2}-\d{2}\b/, "lastupdate: #{todayStr}")
+      new_contents = new_contents.gsub(/\bbuild: \d+\b/, "build: #{wowbuild}")
+      File.open(file, "w") {|out| out.puts new_contents }
+      puts 'Updated!'
+    end
+  end
+end
+
+Dir.glob("relics/*.html").each do |file|
+  puts "Checking #{file}"
+  text = File.read(file)
+  updatedFiles.each do |csv|
+    if text.include?(csv)
+      new_contents = text.gsub(/\bcharttitle: .*\b(\d{4}-\d{2}-\d{2})\b/) { |m| m.gsub!($1, "#{todayStr}") }
+      File.open(file, "w") {|out| out.puts new_contents }
+      puts 'Updated!'
+    end
+  end
+end
+
+Dir.glob("trinkets/*.html").each do |file|
+  puts "Checking #{file}"
+  text = File.read(file)
+  updatedFiles.each do |csv|
+    if text.include?(csv)
+      new_contents = text.gsub(/\bcharttitle: .*\b(\d{4}-\d{2}-\d{2})\b/) { |m| m.gsub!($1, "#{todayStr}") }
+      File.open(file, "w") {|out| out.puts new_contents }
+      puts 'Updated!'
+    end
+  end
+end
+
+puts
+puts 'Done! Press enter to quit...'
+gets
